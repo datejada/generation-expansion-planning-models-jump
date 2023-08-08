2682127ede436406fbff04687401e50c5c75b60c
Diego Alejandro Tejada Arango <12887482+datejada@users.noreply.github.com>
2023-08-08 08:54:31 +0200

    Add initial files
    Add files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generation Expansion Planning (GEP) models considering uncertainties on renewable energy resources (RES) using Julia/JuMP
+
+The following files solve the GEP problem for three scenarios of wind and solar production using different approaches:
+
+* **Stochastic-GEP.jl**: Two-Stage Stochastic Generation Expansion Planning
+
+The models are developed in [JuMP](https://jump.dev/JuMP.jl/stable/) v1.8.0 and solved using [HiGHS](https://highs.dev/) v1.4.3.
+
+The main references to model the optimization problems are:
+
+[1] [Optimization Techniques by Andrés Ramos Galán](https://pascua.iit.comillas.edu/aramos/OT.htm)
+
+[2] [Good Optimization Modeling Practices using Pyomo](https://pascua.iit.comillas.edu/aramos/simio/transpa/s_GoodOptimizationModelingPracticesPyomo.pdf)
---
+++ Stochastic-GEP.jl
@@ -0,0 +1,84 @@
+# Two-Stage Stochastic Generation Expansion Planning
+
+# Packages
+using JuMP
+using HiGHS
+using CSV
+using DataFrames
+
+# Folders names
+const INPUT_FOLDER  = joinpath(@__DIR__, "inputs")
+const OUTPUT_FOLDER = joinpath(@__DIR__, "outputs")
+
+# Files names
+demand_file       = joinpath(INPUT_FOLDER, "iGEP_Data_Demand.csv")
+generation_file   = joinpath(INPUT_FOLDER, "iGEP_Data_Generation.csv")
+availability_file = joinpath(INPUT_FOLDER, "iGEP_Data_Availability.csv") 
+scenario_file     = joinpath(INPUT_FOLDER, "iGEP_Data_Scenario.csv")
+
+# Read data
+demand_df       = CSV.read(demand_file, DataFrames.DataFrame)
+generation_df   = CSV.read(generation_file, DataFrames.DataFrame)   
+availability_df = CSV.read(availability_file, DataFrames.DataFrame)
+scenario_df     = CSV.read(scenario_file, DataFrames.DataFrame)      
+
+# Scalar parameters
+p_rp_weight = 365   #weight of representative period [days]
+p_ens_cost  = 0.180 #energy not supplied cost    [kEUR/MWh]
+
+# Sets
+P  = demand_df.p     #time periods (e.g., hours)
+SC = scenario_df.sc  #scenarios
+G  = generation_df.g #generation units
+
+# Parameters
+p_availability    = Dict((row.sc, row.g, row.p) => row.pAviProf for row in eachrow(availability_df)) #availability profile [p.u.]
+p_demand          = Dict(               (row.p) => row.pDemand  for row in eachrow(demand_df))       #demand per time period [MW]
+p_investment_cost = Dict(        (row.g)        => row.pInvCost for row in eachrow(generation_df))   #investment cost of generation units [kEUR/MW/year]
+p_variable_cost   = Dict(        (row.g)        => row.pVarCost for row in eachrow(generation_df))   #variable   cost of generation units [kEUR/MWh]
+p_unit_capacity   = Dict(        (row.g)        => row.pUnitCap for row in eachrow(generation_df))   #capacity        of generation units [MW]
+p_sc_prob         = Dict((row.sc)               => row.pScProb  for row in eachrow(scenario_df))     #probability of scenario [p.u.]
+
+# Model
+model = Model(HiGHS.Optimizer)
+
+# Variables
+@variable(model, 0 ≤ v_production[SC,G,P])           #production [MW] 
+@variable(model, 0 ≤ v_ens[SC,p in P] ≤ p_demand[p]) #energy not supplied [MW]
+@variable(model, 0 ≤ v_investment[G],Int)            #number of installed generation units [N]
+
+# Expressions
+e_investment_cost = @expression(model, sum(p_investment_cost[g] * p_unit_capacity[g] * v_investment[g] for g in G))
+e_variable_cost   = @expression(model, p_rp_weight * sum(p_sc_prob[sc] * p_variable_cost[g] * v_production[sc,g,p] for sc in SC, g in G, p in P))
+e_ens_cost        = @expression(model, p_rp_weight * sum(p_sc_prob[sc] * p_ens_cost * v_ens[sc,p] for sc in SC, p in P))
+
+# Objective function
+@objective(model, Min, e_investment_cost + e_variable_cost + e_ens_cost)
+
+# Constraints
+# - balance equation
+@constraint(model, c_balance[sc in SC, p in P],
+                sum(v_production[sc,g,p] for g in G) + v_ens[sc,p] == p_demand[p]
+            )
+
+# - maximum generation
+@constraint(model, c_max_prod[sc in SC, g in G, p in P],
+                v_production[sc,g,p] <= get(p_availability, (sc, g, p), 1.0) * p_unit_capacity[g]*v_investment[g]
+            )
+
+# print lp file
+write_to_file(model, "model.lp")
+
+# Solve model
+optimize!(model)
+
+# Objective function value
+println("Total cost: ", objective_value(model))
+
+# Writing the investment results to a CSV file
+output_file = open(joinpath(OUTPUT_FOLDER, "oGEP_Invest_Result.csv"), "w")
+write(output_file, "g,InstalUnits,InstalCap_MW\n")
+for g in G
+    write(output_file, "$g,$(value(v_investment[g])),$(value(p_unit_capacity[g]) * value(v_investment[g]))\n")
+end
+close(output_file)
