7db5b4be1dad4c7d61e497ea0b0bedeb25986a59
datejada <diego.tejadaarango@tno.nl>
2024-03-31 21:07:58 +0200

    Add function to plot investment at each iteration

--- Stochastic-GEP-two-stage-Benders-multicut.jl
+++ Stochastic-GEP-two-stage-Benders-multicut.jl
@@ -311,6 +311,32 @@ function plot_benders_bounds(df_Benders_interations)
 end
 
 """
+    plot_investment_per_iteration(df_investment_per_iteration)
+
+Plot the investment per iteration.
+
+# Arguments
+- `df_investment_per_iteration`: A DataFrame containing the investment per iteration.
+
+# Returns
+- `p`: A plot object showing the investment per iteration.
+
+"""
+function plot_investment_per_iteration(df_investment_per_iteration)
+    p = @df df_investment_per_iteration plot(
+        :iteration,
+        :investment,
+        group = :generator,
+        xlabel = "Iteration",
+        ylabel = "Investment [units]",
+        title = "Investment per Iteration",
+        lw = 2,
+        xticks = 0:1:MAXIMUM_ITERATIONS,
+    )
+    return p
+end
+
+"""
     plot_investment(model)
 
 Plot the investment in generation units.
@@ -519,4 +545,3 @@ function save_results_subproblem_to_csv(output_folder, model)
 
     return nothing
 end
-
--- Stochastic-GEP-two-stage-Benders.jl
+++ Stochastic-GEP-two-stage-Benders.jl
@@ -302,6 +302,32 @@ function plot_benders_bounds(df_Benders_interations)
 end
 
 """
+    plot_investment_per_iteration(df_investment_per_iteration)
+
+Plot the investment per iteration.
+
+# Arguments
+- `df_investment_per_iteration`: A DataFrame containing the investment per iteration.
+
+# Returns
+- `p`: A plot object showing the investment per iteration.
+
+"""
+function plot_investment_per_iteration(df_investment_per_iteration)
+    p = @df df_investment_per_iteration plot(
+        :iteration,
+        :investment,
+        group = :generator,
+        xlabel = "Iteration",
+        ylabel = "Investment [units]",
+        title = "Investment per Iteration",
+        lw = 2,
+        xticks = 0:1:MAXIMUM_ITERATIONS,
+    )
+    return p
+end
+
+"""
     plot_investment(model)
 
 Plot the investment in generation units.
